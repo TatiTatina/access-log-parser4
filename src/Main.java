010c4b2476918e1ac3053c4b427306692fe44842
TatiTatina <Romanovskaya-Tanya@yandex.ru>
2024-09-13 15:45:40 +0700

    Создана ветка 2-number-experiments, внесены изменения в Main

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        System.out.println("Введите текс и нажмите <Enter>");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        System.out.println("Введите первое число число: ");
+        int firstNumber  = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число число: ");
+        int secondNumber  = new Scanner(System.in).nextInt();
+
+        int summa =  firstNumber+secondNumber;
+        System.out.println("Сумма: " + summa);
+
+        int raznost =  firstNumber-secondNumber;
+        System.out.println("Разность: " + raznost);
+
+        int proizvedenie =  firstNumber*secondNumber;
+        System.out.println("Произведение: " + proizvedenie);
+
+        double quotient = (double) firstNumber/secondNumber;
+        System.out.println("Частное от деления: " + quotient);
     }
 }
